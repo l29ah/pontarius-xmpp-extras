d802911ea66985ff1ec9abcfe1c8933060dd5987
Sergey Alirzaev <zl29ah@gmail.com>
2020-06-12 05:33:36 +0300

    improved Date/Time serialization, added parsing

--- Network/Xmpp/Extras/DateTime.hs
+++ Network/Xmpp/Extras/DateTime.hs
@@ -7,6 +7,8 @@ module Network.Xmpp.Extras.DateTime where
 
 import Data.Time
 
--- |No milliseconds yet as `formatTime` lacks the required feature
 toDateTime :: UTCTime -> String
-toDateTime t = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ" t
+toDateTime = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ"
+
+fromDateTime :: String -> Maybe UTCTime
+fromDateTime = parseTimeM False defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ"
